5449a1aca41993a428943a99b0307cbf197bd0a2
it21294198 <99207728+it21294198@users.noreply.github.com>
2022-02-22 11:15:55 +0530

    Update exercise02.cpp

--- exercise02.cpp
+++ exercise02.cpp
@@ -29,5 +29,8 @@ int main() {
     
     return 0;
 }
-
+int volume(int height, int width, int length){
+    int vol=height*width*length;
+    return vol;  
+}
 // Implement the Volume() function here
